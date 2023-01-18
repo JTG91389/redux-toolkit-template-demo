e06eac6cfedbfcb76db33026ad961dad4b728e6e
JTG91389 <jgear91389@gmail.com>
2023-01-17 20:58:50 -0600

    style clean up

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -8,7 +8,11 @@ import {
   incrementIfOdd,
   selectCount,
 } from './counterSlice';
-import '../../Counter.module.css'; // stackblitz component style render workaround
+/** import styles from './Counter.module.css';
+ * You'll see the original styles object usage commented out below
+ */
+import '../../Counter.module.css';
+// stackblitz component style render workaround
 
 export function Counter() {
   const count = useSelector(selectCount);
@@ -19,16 +23,20 @@ export function Counter() {
 
   return (
     <div>
+      {/* <div className={styles.row}> */}
       <div className="row">
         <button
+          // className={styles.button}
           className="button"
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
+        {/* <span className={styles.value}>{count}</span> */}
         <span className="value">{count}</span>
         <button
+          // className={styles.button}
           className="button"
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
@@ -36,26 +44,31 @@ export function Counter() {
           +
         </button>
       </div>
+      {/* <div className={styles.row}> */}
       <div className="row">
         <input
+          // className={styles.textbox}
           className="textbox"
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
+          // className={styles.button}
           className="button"
           onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
           Add Amount
         </button>
         <button
+          // className={styles.asyncButton}
           className="asyncButton"
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
           Add Async
         </button>
         <button
+          // className={styles.button}
           className="button"
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
