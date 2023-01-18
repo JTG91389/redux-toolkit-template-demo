fcc6154cfa3e31a28adf915d229432105535a0cb
JTG91389 <jgear91389@gmail.com>
2023-01-17 20:30:48 -0600

    stack blitz friendly app changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { store } from './src/app/store';
+import App from './src/App';
+import reportWebVitals from './src/reportWebVitals';
 import './index.css';
 
 const container = document.getElementById('root');
